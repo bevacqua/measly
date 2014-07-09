7de973eba7058893442a425943216b5e017cbd37
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-07-09 14:51:59 -0300

    Release v0.1.3

--- dist/measly.js
+++ dist/measly.js
@@ -1,6 +1,6 @@
 /**
  * measly - A measly wrapper around XHR to help you contain your requests
- * @version v0.1.2
+ * @version v0.1.3
  * @link https://github.com/bevacqua/measly
  * @license MIT
  */
