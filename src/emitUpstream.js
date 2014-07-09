a1c94a9cbee99b80b1406079fcfe3eb69de67692
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-07-09 20:22:37 -0300

    ugh

--- src/emitUpstream.js
+++ src/emitUpstream.js
@@ -2,16 +2,14 @@
 
 module.exports = function (emitter, context, types) {
   types.forEach(function through (type) {
-    emitter.on(type, raise);
+    emitter.on(type, function raise () {
+      var args = Array.prototype.slice.call(arguments);
+      var all = [type].concat(args);
+      var ctx = context;
+      while (ctx) {
+        ctx.emit.apply(emitter, all);
+        ctx = ctx.parent;
+      }
+    });
   });
-
-  function raise () {
-    var args = Array.prototype.slice.call(arguments);
-    var all = [type].concat(args);
-    var ctx = context;
-    while (ctx) {
-      ctx.emit.apply(emitter, all);
-      ctx = ctx.parent;
-    }
-  }
 };
